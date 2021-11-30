0dcc4b0601233efcd1e035cf525cbb4fa5f6ec35
sagar8995 <95270620+sagar8995@users.noreply.github.com>
2021-11-30 18:13:32 +0530

    sagar
    commit

--- venv/Scripts/Credkeeper.py
+++ venv/Scripts/Credkeeper.py
@@ -2,6 +2,7 @@ import time
 import logging
 from selenium import webdriver
 import selenium
+from selenium.webdriver.support.select import Select
 
 
 
@@ -41,39 +42,45 @@ driver.find_element_by_id("autoshare").send_keys("Yes")
 
 driver.find_element_by_class_name("ngx-dropdown-button").click()
 
-obj = select(driver.find_element_by_name("search-text").send_keys("Sagar Soni"))
-obj.select_by_visible_text(" Sagar Soni ")
-
-
-
+driver.find_element_by_name("search-text").send_keys("Sagar Soni")
+## time.sleep(5)
+# driver.find_element_by_css_selector("available-items li[xpath='1']").click()
+# select_by_value(-1)
+# driver.find_element_by_class_name("ng-star-inserted").click()
+# print(len(names))
+# for name in names
+#     if search-text == "Sagar soni":
+#         search-text.click()
+#         break
+# input[class="ng-pristine ng-valid ng-touched"]
+# countries = driver.find_elements_by_css_selector("li[class='ui-menu-item'] a")
+# print(len(countries))
+# for country in countries:
+#     if country.text == "Sagar soni":
+#         country.click()
+#             break
 # time.sleep(4)
-# element.click()
-
+## element.click()
 driver.find_element_by_id("articleStatus").send_keys(" Approve To Article Staging ")
-
 driver.find_element_by_id("dummy_date").send_keys("Last 3 days")
-
-
-
-
 # dropdown.select_by_visible_text("CSM")
 # dropdown.select-by_index(1)
-
-
-
-
 #driver.find_elements_by_id("category").send_keys("CSM")
-
-
-
-
-
 #dropdown.select_by_visible-text("PSRE")
-
-
-
 #dropdown.select_by_index(0)
 #dropdown.select-by_value( "PSRE")
+
+
+
+driver.find_element_by_xpath("/html/body/app-root/div/div/app-admin-article-list/div/div/div[2]/div/div[1]/div[7]/a[3]").click()
+driver.find_element_by_name("content_type").send_keys("Image")
+
+driver.find_element_by_xpath("/html/body/app-root/div/div/app-admin-article/div/div/div[2]/div/form/div[2]/div/select").send_keys("CSM")
+
+driver.find_element_by_class_name("form-control ng-touched ng-dirty ng-valid").send_keys("CSM")
+
+driver.find_element_by_name("title").send_keys("Sagar")
+
 
 
 
